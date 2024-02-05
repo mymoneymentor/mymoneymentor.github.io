b7af4a993c7af6fb2d3b22e5ebea64284db977d1
mymoneymentor <134763715+mymoneymentor@users.noreply.github.com>
2024-02-05 20:51:56 +0530

    Fix routing url

--- js/submitform.js
+++ js/submitform.js
@@ -11,7 +11,7 @@ form.addEventListener("submit", async (event) => {
   }
   let payload = {
     phone_number: data.get("phone"),
-    name: data.get("name")
+    name: data.get("name"),
   };
   const other_params = {
     headers: { "content-type": "application/json; charset=UTF-8" },
@@ -32,36 +32,33 @@ form.addEventListener("submit", async (event) => {
     const resData = await res.json();
 
     console.log(resData);
-    alert("Thanks for sharing the information. You'll now be directed to the bank's site to complete the application process.");
-    redirectToBank(sourceBank)
+    alert(
+      "Thanks for sharing the information. You'll now be directed to the bank's site to complete the application process."
+    );
+    redirectToBank(sourceBank);
   } catch (err) {
     alert("You request has been submitted successfully.");
     window.location.href = "https://mymoneymentor.co/contact.html";
   }
 });
 
-
-function redirectToBank(bankname){
-    if(bankname.toLowerCase()=='au'){
-       window.location.href("https://cconboarding.aubank.in/auccself/#/landing?utm_source=dsa&utm_medium=display-agg&utm_campaign=credit-card-dsa-campaign-988137-347803-digi3")
-    }
-    else if(bankname.toLowerCase()=='hdfc'){
-        window.location.href("https://applyonline.hdfcbank.com/cards/credit-cards.html?CHANNELSOURCE=TDCC&DSACode=XSBC&LGcode=DGPI&LCcode=XSBC&LC2=FAZ01&SMcode=S22411#nbb")
-    }
-    else if(bankname.toLowerCase()=='rbl'){
-        window.location.href = "https://www.bajajfinserv.in/apply-for-rbl-bank-credit-card-online?utm_source=mentor_bfl12&utm_medium=mentor_bfl12";
-
-    }
-    else if(bankname.toLowerCase()=='idfc'){
-        window.location.href = "https://www.idfcfirstbank.com/credit-card/ntb-diy/apply?utm_source=Partner&utm_medium=Mentor2&utm_campaign=NTB_MentorCamp";
-
-    }
-    else if(bankname.toLowerCase()=='axis'){
-        window.location.href = "https://clctab.axisbank.co.in/DigitalChannel/WebForm/?ipa197";
-
-    }
-    else{
-        window.location.href = "https://mymoneymentor.co/contact.html";
-    }
-    
+function redirectToBank(bankname) {
+  if (bankname.toLowerCase() == "au") {
+    window.location.href =
+      "https://cconboarding.aubank.in/auccself/#/landing?utm_source=dsa&utm_medium=display-agg&utm_campaign=credit-card-dsa-campaign-988137-347803-digi3";
+  } else if (bankname.toLowerCase() == "hdfc") {
+    window.location.href =
+      "https://applyonline.hdfcbank.com/cards/credit-cards.html?CHANNELSOURCE=TDCC&DSACode=XSBC&LGcode=DGPI&LCcode=XSBC&LC2=FAZ01&SMcode=S22411#nbb";
+  } else if (bankname.toLowerCase() == "rbl") {
+    window.location.href =
+      "https://www.bajajfinserv.in/apply-for-rbl-bank-credit-card-online?utm_source=mentor_bfl12&utm_medium=mentor_bfl12";
+  } else if (bankname.toLowerCase() == "idfc") {
+    window.location.href =
+      "https://www.idfcfirstbank.com/credit-card/ntb-diy/apply?utm_source=Partner&utm_medium=Mentor2&utm_campaign=NTB_MentorCamp";
+  } else if (bankname.toLowerCase() == "axis") {
+    window.location.href =
+      "https://clctab.axisbank.co.in/DigitalChannel/WebForm/?ipa197";
+  } else {
+    window.location.href = "https://mymoneymentor.co/contact.html";
+  }
 }
